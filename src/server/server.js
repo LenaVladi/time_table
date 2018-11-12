a1b94088e8ed3b03553292c220b86df6d3bc22a2
alena.siprikova <alena.siprikova@defa.ru>
2018-11-12 12:01:03 +0300

    corrected encoding

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,13 @@
-const Koa = require('koa'); // фреймворк коа
-const axios = require('axios'); // для запросов
-const Router = require('koa-router'); // для маршрутов в данном случае /
-const cors = require('@koa/cors'); // для заголовков корс
-const koaBody = require('koa-body'); // для получения параметров запроса
+const Koa = require('koa'); // С„СЂРµР№РјРІРѕСЂРє РєРѕР°
+const axios = require('axios'); // РґР»СЏ Р·Р°РїСЂРѕСЃРѕРІ
+const Router = require('koa-router'); // РґР»СЏ РјР°СЂС€СЂСѓС‚РѕРІ РІ РґР°РЅРЅРѕРј СЃР»СѓС‡Р°Рµ /
+const cors = require('@koa/cors'); // РґР»СЏ Р·Р°РіРѕР»РѕРІРєРѕРІ РєРѕСЂСЃ
+const koaBody = require('koa-body'); // РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РїР°СЂР°РјРµС‚СЂРѕРІ Р·Р°РїСЂРѕСЃР°
 const app = new Koa();
 
 var router = new Router();
 
-router.get('/', koaBody(), async (ctx, next) => { // get('/') - адрес по которому обращаться
+router.get('/', koaBody(), async (ctx, next) => { // get('/') - Р°РґСЂРµСЃ РїРѕ РєРѕС‚РѕСЂРѕРјСѓ РѕР±СЂР°С‰Р°С‚СЊСЃСЏ
   const { query } = ctx.request;
   const data = await axios.get('https://api.rasp.yandex.net/v3.0/schedule/', {
     params: query,
@@ -23,4 +23,4 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(3000); // порт 3000
+app.listen(3000); // РїРѕСЂС‚ 3000
